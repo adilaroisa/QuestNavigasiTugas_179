75b15408edd098d11cb2b556f71c69f5b13beeda
adilaroisa <adilaroisa688@gmail.com>
2025-11-08 20:18:38 +0700

    menambahkan teks sambutan "Selamat Datang" pada halaman Home

--- app/src/main/java/com/example/tugaspertemuan6/view/HalamanHome.kt
+++ app/src/main/java/com/example/tugaspertemuan6/view/HalamanHome.kt
@@ -26,4 +26,21 @@ fun HalamanHome(
                     .padding(vertical = dimensionResource(id = R.dimen.padding_xlarge)),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.SpaceBetween
-            )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.selamat_datang),
+                    fontSize = dimensionResource(id = R.dimen.font_size_header).value.sp,
+
+                    fontWeight = FontWeight.SemiBold,
+                    color = colorResource(id = R.color.dark_purple_text)
+                )
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.spacer_xlarge)))
+
+                Image(
+                    painter = painterResource(id = R.drawable.welcome),
+                    contentDescription = stringResource(id = R.string.logo_content_desc),
+                    modifier = Modifier.size(dimensionResource(id = R.dimen.logo_size))
+                )
+
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.spacer_xlarge)))
+
