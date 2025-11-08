97f1b16039ffe451439e93df207ffd0a5c1643ef
adilaroisa <adilaroisa688@gmail.com>
2025-11-08 20:57:42 +0700

    membuat tampilan kartu peserta menggunakan PesertaCard composable

--- app/src/main/java/com/example/tugaspertemuan6/view/ListPeserta.kt
+++ app/src/main/java/com/example/tugaspertemuan6/view/ListPeserta.kt
@@ -108,3 +108,48 @@ fun ListPesertaScreen(
 
 
 
+@Composable
+fun PesertaCard(peserta: PesertaData) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = dimensionResource(id = R.dimen.card_elevation_small)
+        ),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                DataLabelValue(
+                    label = stringResource(id = R.string.label_nama_lengkap),
+                    value = peserta.nama,
+                    modifier = Modifier.weight(1f)
+                )
+                DataLabelValue(
+                    label = stringResource(id = R.string.label_jenis_kelamin),
+                    value = peserta.jenisKelamin,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_small)))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                DataLabelValue(
+                    label = stringResource(id = R.string.label_status),
+                    value = peserta.statusPerkawinan,
+                    modifier = Modifier.weight(1f)
+                )
+                DataLabelValue(
+                    label = stringResource(id = R.string.label_alamat),
+                    value = peserta.alamat,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+    }
+}
