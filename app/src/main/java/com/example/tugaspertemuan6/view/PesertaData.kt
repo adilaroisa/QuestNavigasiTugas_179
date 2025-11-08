d3491998f5c5fd9c19f3ba815e38edf5dd72d5dc
adilaroisa <adilaroisa688@gmail.com>
2025-11-08 20:42:12 +0700

    mendefinisikan data class PesertaData

---
+++ app/src/main/java/com/example/tugaspertemuan6/view/PesertaData.kt
@@ -0,0 +1,8 @@
+package com.example.tugaspertemuan6.view
+
+data class PesertaData(
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val statusPerkawinan: String = "",
+    val alamat: String = ""
+)
